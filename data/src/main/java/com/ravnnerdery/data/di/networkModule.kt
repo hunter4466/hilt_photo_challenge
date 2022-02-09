8cef1d78f723e10c524d353d77c1be5f4b44cb90
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-09 15:59:27 -0500

    Hilt: Setup ViewModel anotation and dependencies injection

--- data/src/main/java/com/ravnnerdery/data/di/networkModule.kt
+++ data/src/main/java/com/ravnnerdery/data/di/networkModule.kt
@@ -1,8 +1,39 @@
 package com.ravnnerdery.data.di
 
-import com.ravnnerdery.photo_challenge.network.PhotosApi
-import org.koin.dsl.module
+import com.ravnnerdery.photo_challenge.network.PhotosApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
-val networkModule = module {
-    factory { PhotosApi }
+@Module
+@InstallIn(SingletonComponent::class)
+object NetWorkModule {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Singleton
+    @Provides
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun getPhotosAPI(retrofit: Retrofit): PhotosApiService {
+        val retrofitService: PhotosApiService by lazy {
+            retrofit.create(PhotosApiService::class.java)
+        }
+        return retrofitService
+    }
+
 }
