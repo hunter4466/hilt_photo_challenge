21d5c53f4ea326db869123cc20e1ce909504d131
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-09 16:00:27 -0500

    Hilt: Setup useCases injection configuration

--- data/src/main/java/com/ravnnerdery/data/useCases/GetAllPhotosUseCase.kt
+++ data/src/main/java/com/ravnnerdery/data/useCases/GetAllPhotosUseCase.kt
@@ -2,12 +2,13 @@ package com.ravnnerdery.data.useCases
 
 import com.ravnnerdery.domain.repository.MainRepository
 import com.ravnnerdery.domain.models.PhotoInfo
+import javax.inject.Inject
 
 interface GetAllPhotosUseCase {
     suspend fun execute(): MutableList<PhotoInfo>
 }
 
-class GetAllPhotosUseCaseImpl(private val repo: MainRepository) : GetAllPhotosUseCase {
+class GetAllPhotosUseCaseImpl @Inject constructor(private val repo: MainRepository) : GetAllPhotosUseCase {
     override suspend fun execute(): MutableList<PhotoInfo> {
         return repo.allPhotosFromDatabase()
     }
--- data/src/main/java/com/ravnnerdery/data/useCases/LoadApiToDbUseCase.kt
+++ data/src/main/java/com/ravnnerdery/data/useCases/LoadApiToDbUseCase.kt
@@ -1,12 +1,13 @@
 package com.ravnnerdery.data.useCases
 
 import com.ravnnerdery.domain.repository.MainRepository
+import javax.inject.Inject
 
 interface LoadApiToDbUseCase {
     fun execute()
 }
 
-class LoadApiToDbUseCaseImpl(private val repo: MainRepository) : LoadApiToDbUseCase {
+class LoadApiToDbUseCaseImpl @Inject constructor(private val repo: MainRepository) : LoadApiToDbUseCase {
     override fun execute() {
         repo.loadFromApiAndSetIntoDatabase()
     }
