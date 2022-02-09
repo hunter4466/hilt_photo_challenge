934a9da3d19e513e37b1e17e00541cda8d7e0ace
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-09 16:01:00 -0500

    Hilt: Setup interface injection for network consuming

--- data/src/main/java/com/ravnnerdery/data/networking/PhotosApi.kt
+++ data/src/main/java/com/ravnnerdery/data/networking/PhotosApi.kt
@@ -8,22 +8,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
-private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL)
-    .build()
-
-interface PostsApiService {
+interface PhotosApiService {
     @GET("photos")
     fun getPhotos(): Call<List<PhotoInfoResponse>>
 }
 
-object PhotosApi {
-    val retrofitService: PostsApiService by lazy {
-        retrofit.create(PostsApiService::class.java)
-    }
-}
