21de691a5a95f282b1fbad034877bf499ce04d64
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-09 15:55:35 -0500

    Hilt: Setup hilt dependencies for all modules

--- app/src/main/java/com/ravnnerdery/cleanphotochallenge/di/appModule.kt
+++ app/src/main/java/com/ravnnerdery/cleanphotochallenge/di/appModule.kt
@@ -1,11 +1,29 @@
-package com.ravnnerdery.cleanphotochallenge.di
-
-import com.ravnnerdery.cleanphotochallenge.viewModels.EnlargedPhotoViewModel
-import com.ravnnerdery.cleanphotochallenge.viewModels.PhotoListViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val appModule = module {
-    viewModel { PhotoListViewModel(get()) }
-    viewModel { EnlargedPhotoViewModel(get()) }
-}
+//package com.ravnnerdery.cleanphotochallenge.di
+//
+//import androidx.lifecycle.ViewModel
+//import androidx.recyclerview.widget.RecyclerView
+//import com.ravnnerdery.cleanphotochallenge.viewModels.EnlargedPhotoViewModel
+//import com.ravnnerdery.cleanphotochallenge.viewModels.PhotoListViewModel
+//import com.ravnnerdery.data.useCases.GetAllPhotosUseCase
+//import dagger.Module
+//import dagger.Provides
+//import dagger.hilt.InstallIn
+//import dagger.hilt.components.SingletonComponent
+//
+////import org.koin.androidx.viewmodel.dsl.viewModel
+////import org.koin.dsl.module
+////
+////val appModule = module {
+////    viewModel { PhotoListViewModel(get()) }
+////    viewModel { EnlargedPhotoViewModel(get()) }
+////}
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object ViewModelsModule {
+//    @Provides
+//    fun provideEnlargedPhotoViewModel(getAllPhotosUseCase: GetAllPhotosUseCase): ViewModel{
+//        return getAllPhotosUseCase.execute()
+//    }
+//}
+//
+//
