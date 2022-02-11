24c8625038b8c721ec35f69f2e410c7b580f19fa
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-11 03:01:24 -0500

    RxJava: Update Fragment with some RxAndroid fix

--- app/src/main/java/com/ravnnerdery/cleanphotochallenge/views/PhotoListFragment.kt
+++ app/src/main/java/com/ravnnerdery/cleanphotochallenge/views/PhotoListFragment.kt
@@ -1,10 +1,12 @@
 package com.ravnnerdery.cleanphotochallenge.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearSnapHelper
@@ -13,15 +15,21 @@ import com.ravnnerdery.cleanphotochallenge.viewModels.PhotoListViewModel
 import com.ravnnerdery.cleanphotochallenge.adapters.PhotosAdapter
 import com.ravnnerdery.cleanphotochallenge.adapters.viewHolders.photoListViewHolder.PhotoClickListener
 import com.ravnnerdery.cleanphotochallenge.databinding.PhotoListFragmentBinding
+import com.ravnnerdery.domain.models.PhotoInfo
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 @AndroidEntryPoint
-class PhotoListFragment: Fragment() {
+class PhotoListFragment : Fragment() {
 
     private val photoListViewModel: PhotoListViewModel by viewModels()
     private var binding: PhotoListFragmentBinding? = null
     private lateinit var adapter: PhotosAdapter
     private lateinit var snapHelper: SnapHelper
+    private lateinit var disposable: Disposable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +43,8 @@ class PhotoListFragment: Fragment() {
             snapHelper = LinearSnapHelper()
             snapHelper.attachToRecyclerView(it.photoListRecyclerView)
             it.photoListSwypeContainer.setOnRefreshListener {
-                photoListViewModel.allPhotos()
+                Toast.makeText(context,"Updating Posts",Toast.LENGTH_SHORT).show()
+                photoListViewModel.refreshData()
             }
         }
         return binding?.root
@@ -43,18 +52,39 @@ class PhotoListFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        photoListViewModel.mutableAllPhotos.observe(viewLifecycleOwner, {
-            adapter.submitList(it)
-            if (photoListViewModel.currentPosition != 0) {
-                photoListViewModel.currentPosition?.let { it1 ->
-                    binding?.photoListRecyclerView?.scrollToPosition(
-                        it1
-                    )
+        val allPhotosObserver = object : Observer<List<PhotoInfo>> {
+            override fun onSubscribe(d: Disposable) {
+                disposable = d
+            }
+
+            override fun onNext(t: List<PhotoInfo>) {
+                adapter.submitList(t)
+                if (photoListViewModel.currentPosition != 0) {
+                    photoListViewModel.currentPosition?.let { it1 ->
+                        binding?.photoListRecyclerView?.scrollToPosition(
+                            it1
+                        )
+                    }
+                }
+                binding?.let{
+                    it.photoListSwypeContainer.isRefreshing = false
+                    it.photoListRecyclerView.scrollToPosition(1)
+                    Toast.makeText(context,"Posts are up to datea",Toast.LENGTH_SHORT).show()
                 }
             }
-            binding?.photoListSwypeContainer?.isRefreshing = false
-        })
 
+            override fun onError(e: Throwable) {
+                e.printStackTrace()
+            }
+
+            override fun onComplete() {
+                Log.v("PLFragObs", "onComplete: ${Thread.currentThread().name}")
+            }
+
+        }
+        photoListViewModel.allPhotosObservable.execute().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(allPhotosObserver)
 
         photoListViewModel.navigateToSnapshot.observe(this, { photo ->
             photo?.let {
@@ -71,6 +101,7 @@ class PhotoListFragment: Fragment() {
             val snapView: View? = snapHelper.findSnapView(it)
             val snapPosition = snapView?.let { position -> it.getPosition(position) }
             photoListViewModel.currentPosition = snapPosition
+            disposable.dispose()
         }
         super.onDestroy()
     }
