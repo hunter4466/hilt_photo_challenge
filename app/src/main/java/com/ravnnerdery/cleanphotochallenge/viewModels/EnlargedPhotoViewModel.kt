2a94e2553151fce6bb81810d3379a4f1979355f8
Mario Chois <mario.chois.navarro@gmail.com>
2022-02-11 02:59:44 -0500

    RxJava: Configure fragment with observers and proper configuration, add some extra nice features to UI

--- app/src/main/java/com/ravnnerdery/cleanphotochallenge/viewModels/EnlargedPhotoViewModel.kt
+++ app/src/main/java/com/ravnnerdery/cleanphotochallenge/viewModels/EnlargedPhotoViewModel.kt
@@ -1,24 +1,12 @@
 package com.ravnnerdery.cleanphotochallenge.viewModels
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.ravnnerdery.data.useCases.GetAllPhotosUseCase
-import com.ravnnerdery.domain.models.PhotoInfo
-import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EnlargedPhotoViewModel @Inject constructor(private val getAllPhotos: GetAllPhotosUseCase) : ViewModel() {
-
+class EnlargedPhotoViewModel @Inject constructor(getAllPhotosUseCase: GetAllPhotosUseCase) : ViewModel() {
     var currentPosition: Int? = 0
-    val mutableAllPhotos = MutableLiveData<List<PhotoInfo>>(emptyList())
-    fun allPhotos() {
-        viewModelScope.launch(Dispatchers.IO) {
-            mutableAllPhotos.postValue(getAllPhotos.execute())
-        }
-    }
+    val allPhotosObservable = getAllPhotosUseCase
 }
--- app/src/main/java/com/ravnnerdery/cleanphotochallenge/views/EnlargedPhotoFragment.kt
+++ app/src/main/java/com/ravnnerdery/cleanphotochallenge/views/EnlargedPhotoFragment.kt
@@ -1,6 +1,7 @@
 package com.ravnnerdery.cleanphotochallenge.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,8 +12,12 @@ import androidx.recyclerview.widget.SnapHelper
 import com.ravnnerdery.cleanphotochallenge.adapters.EnlargedPhotoAdapter
 import com.ravnnerdery.cleanphotochallenge.databinding.EnlargedPhotoFragmentBinding
 import com.ravnnerdery.cleanphotochallenge.viewModels.EnlargedPhotoViewModel
+import com.ravnnerdery.domain.models.PhotoInfo
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 @AndroidEntryPoint
 class EnlargedPhotoFragment : Fragment() {
@@ -21,6 +26,7 @@ class EnlargedPhotoFragment : Fragment() {
     private lateinit var binding: EnlargedPhotoFragmentBinding
     private lateinit var adapter: EnlargedPhotoAdapter
     private lateinit var snapHelper: SnapHelper
+    private lateinit var disposable: Disposable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,24 +45,41 @@ class EnlargedPhotoFragment : Fragment() {
             snapHelper = LinearSnapHelper()
             snapHelper.attachToRecyclerView(it.enlargedPhotoRecyclerView)
         }
-        enlargedPhotoViewModel.allPhotos()
-        enlargedPhotoViewModel.mutableAllPhotos.observe(viewLifecycleOwner, {
-            if (enlargedPhotoViewModel.currentPosition == 0) {
-                if (it.isNotEmpty()) {
-                    adapter.submitList(it)
-                    val argPosition = args.id.toInt() - 1
-                    binding.enlargedPhotoRecyclerView.scrollToPosition(argPosition)
-                    enlargedPhotoViewModel.currentPosition = argPosition
-                }
-            } else {
-                adapter.submitList(it)
-                enlargedPhotoViewModel.currentPosition?.let { it ->
-                    binding.enlargedPhotoRecyclerView.scrollToPosition(
-                        it
-                    )
+
+        val allPhotosObserver = object: Observer<List<PhotoInfo>> {
+            override fun onSubscribe(d: Disposable) {
+                disposable = d
+            }
+
+            override fun onNext(t: List<PhotoInfo>) {
+                if (enlargedPhotoViewModel.currentPosition == 0) {
+                    if (t.isNotEmpty()) {
+                        adapter.submitList(t)
+                        val argPosition = args.id.toInt() - 1
+                        binding.enlargedPhotoRecyclerView.scrollToPosition(argPosition)
+                        enlargedPhotoViewModel.currentPosition = argPosition
+                    }
+                } else {
+                    adapter.submitList(t)
+                    enlargedPhotoViewModel.currentPosition?.let { it ->
+                        binding.enlargedPhotoRecyclerView.scrollToPosition(
+                            it
+                        )
+                    }
                 }
             }
-        })
+
+            override fun onError(e: Throwable) {
+                e.printStackTrace()
+            }
+
+            override fun onComplete() {
+                Log.v("PLFragObs","onComplete: ${Thread.currentThread().name}" )
+            }
+        }
+        enlargedPhotoViewModel.allPhotosObservable.execute().subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(allPhotosObserver)
     }
 
     override fun onDestroy() {
@@ -64,6 +87,7 @@ class EnlargedPhotoFragment : Fragment() {
             val snapView: View? = snapHelper.findSnapView(it)
             val snapPosition = snapView?.let { position -> it.getPosition(position) }
             enlargedPhotoViewModel.currentPosition = snapPosition
+            disposable.dispose()
         }
         super.onDestroy()
     }
